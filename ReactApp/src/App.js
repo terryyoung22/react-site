4f5453ac88f15e62fe24dc44b31fd52258bf32d6
Terry Young <terryyoung@Terrys-MacBook-Pro.local>
2020-11-04 13:12:36 -0500

    init commit

---
+++ ReactApp/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import greeting from '@terryyoung22/npmcode';
+
+greeting();
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          View <code>-Your Browswers Console-</code> To see message from npm package.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
