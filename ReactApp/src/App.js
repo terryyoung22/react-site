a49c17c62e443125a4ea7351f237c26a6fa6f1d3
terryyoung22 <terryyoung1192@gmail.com>
2020-11-12 23:02:06 -0500

    added history

--- ReactApp/src/App.js
+++ ReactApp/src/App.js
@@ -1,18 +1,19 @@
-<<<<<<< HEAD
 import React, { Component } from 'react';
 import './App.css'; // eslint-disable-next-line
 import { BrowserRouter as Router, Route, Switch, Link, Redirect } from "react-router-dom";
+
 
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
 import Error from './components/Error';
 import Navigation from './components/Navigation';
+import history from './components/history';
 
 class App extends Component {
   render() {
     return (      
-       <Router>
+       <Router history={history}>
         <div>
           <Navigation />
             <Switch>
