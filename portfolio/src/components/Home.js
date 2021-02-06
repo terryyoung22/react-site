67162aeafc09f1c7ba4a76a0f3ce4f59721821bd
terryyoung22 <terryyoung1192@gmail.com>
2021-02-06 01:01:27 -0500

    made changes to code.

--- portfolio/src/components/Home.js
+++ portfolio/src/components/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import logo from '../logo.svg';
-import history from './history';
+// import history from './history';
 // import { Button } from 'react-bootstrap';
 
 export default class Home extends Component {
